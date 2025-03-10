4ec55a0dbad31fba98febdb8a0683421537e5e42
Brylie Christopher Oxley <brylie@protonmail.com>
2025-03-10 21:21:33 +0200

    Add initial BlogIndexPage model with rich text field and migration

--- app/blog/models.py
+++ app/blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+# Add these:
+from wagtail.models import Page
+from wagtail.fields import RichTextField
+
+
+class BlogIndexPage(Page):
+    intro = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + ["intro"]
+    
