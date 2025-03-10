039881aa48904d31524972c7462b5ccbaec29268
Brylie Christopher Oxley <brylie@protonmail.com>
2025-03-10 20:58:31 +0200

    Add initial project structure and core functionality

---
+++ app/core/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-57hsn%1u@e361a!ix-f^4@tp^_01h%q%8#oy$_%h2_k+$q*n*^"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
